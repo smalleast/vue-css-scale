23f5c9998bda9ff4c141c3d23638278628fd3372
zhulogic <38202618@qq.com>
2017-11-09 14:38:59 +0800

    新增浮动在图片的定位面板

--- src/components/vue-css-scale.vue
+++ src/components/vue-css-scale.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="vue-css-scale" :class="[position,className]">
-    <img :src="url" >
+    <img class="scale-img" :src="url">
+    <div class="scale-text" v-if="" :class="[textPosition,textClassName]">
+      <slot name="text"></slot>
+    </div>
   </div>
 </template>
 <script type="text/javascript">
@@ -16,6 +19,13 @@
       },
       url: {
         type: String
+      },
+      textClassName: {
+        type: String
+      },
+      textPosition: {
+        type: String,
+        default: 'none'
       }
     },
     methods: {},
@@ -28,29 +38,78 @@
 <style rel="stylesheet/scss" lang="scss">
   .vue-css-scale {
     position: relative;
-    width: 200px;
-    height: 200px;
+    width: 100%;
+    height: 100%;
     border: 1px solid #ddd;
     overflow: hidden;
-    img {
-      display: inline-block;
-      width: 100%;
-      vertical-align: middle;
-      transition: transform 1s, -webkit-transform 1s;
-    }
     &:hover {
-      img {
-        transform: scale(1.25);
+      .scale {
+        &-img {
+          transform: scale(1.15);
+        }
+        &-text {
+          &.top {
+            transform: translate(0%, 100%);
+          }
+          &.bottom {
+            transform: translate(0%, -100%);
+          }
+          &.left {
+            transform: translate(100%, 0%);
+          }
+          &.right {
+            transform: translate(-100%, -0%);
+          }
+        }
       }
     }
     &.x:hover {
       img {
-        transform: scaleX(1.25);
+        transform: scaleX(1.15);
       }
     }
     &.y:hover {
       img {
-        transform: scaleY(1.25);
+        transform: scaleY(1.15);
+      }
+    }
+    .scale {
+      &-img {
+        position: relative;
+        display: inline-block;
+        width: 100%;
+        vertical-align: middle;
+        transition: transform 1s, -webkit-transform 1s;
+        z-index: 1;
+      }
+      &-text {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.2);
+        z-index: 2;
+        transition: transform .6s, -webkit-transform .6s;
+        &.top {
+          top: -100%;
+        }
+        &.bottom {
+          top: 100%;
+        }
+        &.left {
+          left: -100%;
+        }
+        &.right {
+          left: 100%;
+        }
+        &.normal {
+          left: 0;
+          top: 0;
+        }
+        &.none {
+          display: none;
+        }
       }
     }
   }
--- src/dev/dev.vue
+++ src/dev/dev.vue
@@ -15,6 +15,46 @@
                    url="http://oss.zhulogic.com/product_image/cp083b70a1d989419481acf884557c5a41.jpg?x-oss-process=style/w660">
       </VueCssScale>
     </div>
+    <div class="item">
+      <VueCssScale position="" className="red" textClassName="bg" textPosition="top"
+                   url="http://oss.zhulogic.com/product_image/cp083b70a1d989419481acf884557c5a41.jpg?x-oss-process=style/w660">
+        <div slot="text">
+          <button class="btn-fav">收藏</button>
+        </div>
+      </VueCssScale>
+    </div>
+    <div class="item">
+      <VueCssScale position="" className="red" textClassName="bg" textPosition="right"
+                   url="http://oss.zhulogic.com/product_image/cp083b70a1d989419481acf884557c5a41.jpg?x-oss-process=style/w660">
+        <div slot="text">
+          <button class="btn-fav">收藏</button>
+        </div>
+      </VueCssScale>
+    </div>
+    <div class="item">
+      <VueCssScale position="" className="red" textClassName="bg" textPosition="bottom"
+                   url="http://oss.zhulogic.com/product_image/cp083b70a1d989419481acf884557c5a41.jpg?x-oss-process=style/w660">
+        <div slot="text">
+          <button class="btn-fav">收藏</button>
+        </div>
+      </VueCssScale>
+    </div>
+    <div class="item">
+      <VueCssScale position="" className="red" textClassName="bg" textPosition="left"
+                   url="http://oss.zhulogic.com/product_image/cp083b70a1d989419481acf884557c5a41.jpg?x-oss-process=style/w660">
+        <div slot="text">
+          <button class="btn-fav">收藏</button>
+        </div>
+      </VueCssScale>
+    </div>
+    <div class="item">
+      <VueCssScale position="" className="red" textClassName="bg" textPosition="normal"
+                   url="http://oss.zhulogic.com/product_image/cp083b70a1d989419481acf884557c5a41.jpg?x-oss-process=style/w660">
+        <div slot="text">
+          <button class="btn-fav">收藏</button>
+        </div>
+      </VueCssScale>
+    </div>
   </aside>
 </template>
 <script>
@@ -35,12 +75,36 @@
       margin-right: 20px;
       .red {
         border: 1px solid #f00;
+        width: 200px;
+        height: 200px;
       }
       .green {
         border: 1px solid #0f0;
+        width: 200px;
+        height: 200px;
       }
       .blue {
         border: 1px solid #00f;
+        width: 200px;
+        height: 200px;
+      }
+      .bg {
+        background: rgba(0, 0, 0, .5);
+      }
+      .btn-fav {
+        position: absolute;
+        width: 80px;
+        height: 40px;
+        line-height: 40px;
+        top: 0;
+        right: 0;
+        text-align: center;
+        background: #f00;
+        border-radius: 4px;
+        border: none;
+        font-size: 14px;
+        color: #fff;
+        cursor: pointer;
       }
     }
   }
